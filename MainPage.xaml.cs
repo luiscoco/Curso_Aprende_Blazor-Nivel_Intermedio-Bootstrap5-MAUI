5d36ffb145e06bd64a8d718874327c5df3336247
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-10-04 06:07:22 +0200

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppBootstrap5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
